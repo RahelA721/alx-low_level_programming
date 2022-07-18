dcfa4f1003cf8283ed88bb589f9ed0f46a3bae6f
Rahel Abera <you@example.com>
2022-07-18 12:09:01 -0700

    fills memory with constant bytes

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with constant byte
+ * @s: string to fill
+ * @b: constant byte to fill
+ * @n: memory area to fill
+ *
+ * Return: filled memory area
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
