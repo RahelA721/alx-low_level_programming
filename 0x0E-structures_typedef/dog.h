5840f0a028e0a3148396947c19711f80e3b06cb1
Rahel Abera <you@example.com>
2022-08-02 10:05:20 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - define dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: Define dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
