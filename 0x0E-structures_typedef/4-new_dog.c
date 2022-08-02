0120f4ed7c39d3583904c2d011912cb4d49e7f67
Rahel Abera <you@example.com>
2022-08-02 14:15:09 -0700

    new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * _strlen - length
+ *
+ * @s: string
+ *
+ * Return:len
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{}
+	return (i);
+}
+
+/**
+ * _strcpy - copy
+ * @dest: dest
+ * @src:src
+ *
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int j;
+
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[j] = src[j];
+	}
+	dest[j] = '\0';
+	return (dest);
+}
+
+/**
+ * new_dog - new dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Return: dog_t type
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+	int nl = 0;
+	int ol = 0;
+
+	if (name != NULL && owner != NULL)
+	{
+		nl = _strlen(name) + 1;
+		ol = _strlen(name) + 1;
+		d = malloc(sizeof(dog_t));
+
+		if (d == NULL)
+		{
+			return (NULL);
+		}
+
+		d->name = malloc(sizeof(char) * nl);
+		d->owner = malloc(sizeof(char) * ol);
+		d->name = _strcpy(d->name, name);
+		d->owner = _strcpy(d->owner, owner);
+		d->age = age;
+	}
+	return (d);
+}
+
+
+
