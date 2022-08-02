814d0b4ade94acb13056f37f1a745bdce4017d3e
Rahel Abera <you@example.com>
2022-08-02 14:22:22 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -68,12 +68,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 		d->name = malloc(sizeof(char) * nl);
 		if (d->name == NULL)
 		{
+			free(d);
 			return (NULL);
 		}
 
 		d->owner = malloc(sizeof(char) * ol);
 		if (d->owner == NULL)
 		{
+			free(d);
 			return (NULL);
 		}
 
