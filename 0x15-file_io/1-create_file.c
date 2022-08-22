efc48da6b231c0a8be239e833755be1cee9a402f
Rahel Abera <you@example.com>
2022-08-22 11:07:10 -0700

    creat

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,10 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	write(fd, text_content, _strlen(text_content));
+	if (text_content)
+	{
+		write(fd, text_content, _strlen(text_content));
+	}
 	close(fd);
 	return (1);
 }
